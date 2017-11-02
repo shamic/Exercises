0a8b1a49650b83efb902b623a55f9df5cddc8c7f
Shamic <shamicfan@yeah.net>
2017-11-02 21:49:42 +0800

    Initial Commit

---
+++ Exercises/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Exercises
+//
+//  Created by fancy on 2017/11/2.
+//  Copyright © 2017年 shamic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Exercises/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Exercises
+//
+//  Created by fancy on 2017/11/2.
+//  Copyright © 2017年 shamic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
