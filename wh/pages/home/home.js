76cafacf44f2b4ef9c0fc52b9f362cce794ec3f6
Shamic <shamicfan@yeah.net>
2018-02-23 23:36:23 +0800

    wh

---
+++ wh/pages/home/home.js
@@ -0,0 +1,118 @@
+// pages/home/home.js
+var Bmob = require('../../utils/bmob.js');
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    homeItems: [],
+    banners: [],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    wx.getUserInfo({
+      success: function (res) {
+        var userInfo = res.userInfo
+        var nickName = userInfo.nickName
+        var avatarUrl = userInfo.avatarUrl
+        var gender = userInfo.gender //性别 0：未知、1：男、2：女
+        var province = userInfo.province
+        var city = userInfo.city
+        var country = userInfo.country
+      }
+    })
+
+    var User = Bmob.Object.extend("wh_user");
+    var query = new Bmob.Query(User);
+
+    query.find({
+      success: function (results) {
+        // 循环处理查询到的数据
+        console.log(JSON.stringify(results))
+      },
+      error: function (error) {
+        console.log("查询失败: " + error.code + " " + error.message);
+      }
+    });
+
+    var Banner = Bmob.Object.extend("Banner");
+    var query = new Bmob.Query(Banner);
+
+    query.find({
+      success: function (results) {
+        // 循环处理查询到的数据
+        console.log(JSON.stringify(results))
+        var banners = []
+        for (var i = 0; i < results.length; i++) {
+          var item = results[i]
+          banners.push(item.attributes.img.url)
+        }
+        that.setData({
+          banners: banners
+        })
+      },
+      error: function (error) {
+        console.log("查询失败: " + error.code + " " + error.message);
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
