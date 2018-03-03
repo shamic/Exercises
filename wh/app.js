4cad51d0904ebac63da0df9797e44c7da561a1fa
Shamic <shamicfan@yeah.net>
2018-03-03 21:51:09 +0800

    wh

--- wh/app.js
+++ wh/app.js
@@ -1,6 +1,7 @@
 //app.js
 var Bmob = require('utils/bmob.js');
-Bmob.initialize("28d50890c51c4f0120b96351755d1c11", "93c97fbf0305c4db613bc9ee515c1b8c");
+// Bmob.initialize("28d50890c51c4f0120b96351755d1c11", "93c97fbf0305c4db613bc9ee515c1b8c");
+Bmob.initialize("5d138214843983b024ef2488bb9ac47b", "ea2181f1d144ac7c4e2de5b9abdaa33e");
 
 App({
   onLaunch: function () {
